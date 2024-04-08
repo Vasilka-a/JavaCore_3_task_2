b97a63e158bef1f29f8991cbaca03292d61bf926
Nastya B <nastybondareva@yandex.ru>
2024-04-08 22:52:23 +0300

    Main changed

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-
-        GameProgress gameProgress1 = new GameProgress(50,5,7,250);
-        GameProgress gameProgress2 = new GameProgress(30,6,6,200);
-        GameProgress gameProgress3 = new GameProgress(75,3,5,150);
+        GameProgress gameProgress1 = new GameProgress(50, 5, 7, 250);
+        GameProgress gameProgress2 = new GameProgress(30, 6, 6, 200);
+        GameProgress gameProgress3 = new GameProgress(75, 3, 5, 150);
 
         gameProgress1.saveGame(gameProgress1, "D://Games/savegames/save1.dat");
         gameProgress2.saveGame(gameProgress2, "D://Games/savegames/save2.dat");
@@ -20,5 +19,5 @@ public class Main {
 
         GameProgress.zipFiles("D://Games/savegames/saveGames.zip", filesToZip);
 
-        }
     }
+}
